43422bd724f96f7fb9337e03ceb5db619408dddd
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2025-01-20 16:14:43 +0700

    fixed

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:isolate';
 
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,43 +12,76 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _result = "Waiting...";
-
+  // String _result = "Waiting...";
+  late CarouselController _controller;
+  late PageController _pageController;
+  int _currentIndex = 0;
+  int _lastReportedPage = 0;
+  final List<String> _images = [
+    'https://cdn.pixabay.com/photo/2022/02/01/02/47/smartphone-6985369_1280.png',
+    'https://cdn.martech.zone/wp-content/uploads/2019/01/ios-android-mobile-app-banner.png',
+    'https://twinr.dev/wp-content/uploads/2024/06/1.-Featured-Image-1024x536.webp',
+  ];
   @override
   void initState() {
     super.initState();
 
-    _startIsolate();
-  }
+    // _startIsolate();
+    _controller = CarouselController(initialItem: _currentIndex);
+    _pageController = PageController(initialPage: _currentIndex);
 
-  static void isolateTask(SendPort sendPort) {
-    int result = 0;
-    for (int i = 0; i <= 100000000; i++) {
-      result += i;
-    }
-    sendPort.send(result); // Send the result back to the main isolate
-  }
-
-  Future<void> _startIsolate() async {
-    final ReceivePort receivePort = ReceivePort();
-    try {
-      // Spawn the isolate and pass the sendPort
-      await Isolate.spawn(isolateTask, receivePort.sendPort);
-
-      // Listen for the message from the isolate
-      receivePort.listen((message) {
+    Timer.periodic(Duration(seconds: 10), (timer) {
+      if (_currentIndex < _images.length - 1) {
         setState(() {
-          _result = 'Result from isolate: $message';
+          _currentIndex++;
         });
-        receivePort.close();
-      });
-    } catch (e) {
-      // Handle any error that might occur during isolate spawning
-      setState(() {
-        _result = 'Error occurred: $e';
-      });
-    }
+      } else {
+        setState(() {
+          _currentIndex = 0;
+        });
+      }
+      double offset = _currentIndex * MediaQuery.of(context).size.width;
+      _controller.animateTo(
+        offset,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+      // _pageController.animateToPage(
+      //   _currentIndex,
+      //   duration: Duration(milliseconds: 300),
+      //   curve: Curves.easeInOut,
+      // );
+    });
   }
+
+  // static void isolateTask(SendPort sendPort) {
+  //   int result = 0;
+  //   for (int i = 0; i <= 100000000; i++) {
+  //     result += i;
+  //   }
+  //   sendPort.send(result); // Send the result back to the main isolate
+  // }
+
+  // Future<void> _startIsolate() async {
+  //   final ReceivePort receivePort = ReceivePort();
+  //   try {
+  //     // Spawn the isolate and pass the sendPort
+  //     await Isolate.spawn(isolateTask, receivePort.sendPort);
+
+  //     // Listen for the message from the isolate
+  //     receivePort.listen((message) {
+  //       setState(() {
+  //         _result = 'Result from isolate: $message';
+  //       });
+  //       receivePort.close();
+  //     });
+  //   } catch (e) {
+  //     // Handle any error that might occur during isolate spawning
+  //     setState(() {
+  //       _result = 'Error occurred: $e';
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -64,25 +99,62 @@ class _HomeScreenState extends State<HomeScreen> {
           //   onPressed: _startIsolate,
           //   child: const Text('Start Isolate'),
           // ),
-          SizedBox(
-            height: 300,
-            child: CarouselView.weighted(
-              padding: EdgeInsets.all(10),
-              itemSnapping: true,
-              flexWeights: [3],
-              children: [
-                Image.network(
-                  'https://cdn.pixabay.com/photo/2022/02/01/02/47/smartphone-6985369_1280.png',
-                  fit: BoxFit.cover,
-                ),
-                Image.network(
-                    'https://cdn.martech.zone/wp-content/uploads/2019/01/ios-android-mobile-app-banner.png',
-                    fit: BoxFit.cover),
-                Image.network(
-                    'https://twinr.dev/wp-content/uploads/2024/06/1.-Featured-Image-1024x536.webp',
-                    fit: BoxFit.cover),
-              ],
+          NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              if (notification.depth == 0 &&
+                  notification is ScrollUpdateNotification) {
+                final ScrollMetrics metrics = notification.metrics;
+                final double currentPage =
+                    metrics.pixels / metrics.viewportDimension;
+                final int roundedPage = currentPage.round();
+
+                // If you want to update the page index:
+                if (roundedPage != _lastReportedPage) {
+                  setState(() {
+                    _lastReportedPage = roundedPage;
+                    _currentIndex = roundedPage;
+                  });
+                }
+              }
+              return true;
+            },
+            child: SizedBox(
+              height: 300,
+              child: CarouselView.weighted(
+                controller: _controller,
+                // scrollDirection: Axis.vertical,
+
+                padding: EdgeInsets.all(10),
+                itemSnapping: true,
+                flexWeights: [3],
+                onTap: (value) {
+                  setState(() {
+                    _currentIndex = value;
+                  });
+                },
+                children:
+                    List.generate(growable: true, _images.length, (index) {
+                  return Image.network(_images[index], fit: BoxFit.cover);
+                }),
+              ),
             ),
+          ),
+          AnimatedSmoothIndicator(
+            activeIndex: _currentIndex,
+            // controller: _pageController, // PageController
+            count: _images.length,
+            effect: ExpandingDotsEffect(), // your preferred effect
+            onDotClicked: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+              _controller.animateTo(
+                index * MediaQuery.of(context).size.width,
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+              // _pageController.animateToPage(index, duration: Duration(milliseconds: 300));
+            },
           )
         ],
       ),
