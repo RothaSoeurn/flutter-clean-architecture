dd198e8555ccb774b5ef7b81e59caab4ee74f7c6
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2025-01-20 14:34:39 +0700

    ui

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -58,12 +58,32 @@ class _HomeScreenState extends State<HomeScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(_result, style: const TextStyle(fontSize: 18)),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _startIsolate,
-            child: const Text('Start Isolate'),
-          ),
+          // Text(_result, style: const TextStyle(fontSize: 18)),
+          // const SizedBox(height: 20),
+          // ElevatedButton(
+          //   onPressed: _startIsolate,
+          //   child: const Text('Start Isolate'),
+          // ),
+          SizedBox(
+            height: 300,
+            child: CarouselView.weighted(
+              padding: EdgeInsets.all(10),
+              itemSnapping: true,
+              flexWeights: [3],
+              children: [
+                Image.network(
+                  'https://cdn.pixabay.com/photo/2022/02/01/02/47/smartphone-6985369_1280.png',
+                  fit: BoxFit.cover,
+                ),
+                Image.network(
+                    'https://cdn.martech.zone/wp-content/uploads/2019/01/ios-android-mobile-app-banner.png',
+                    fit: BoxFit.cover),
+                Image.network(
+                    'https://twinr.dev/wp-content/uploads/2024/06/1.-Featured-Image-1024x536.webp',
+                    fit: BoxFit.cover),
+              ],
+            ),
+          )
         ],
       ),
     );
